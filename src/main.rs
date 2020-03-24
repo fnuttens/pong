87f5e2c975bacdfa8d81357106cda08e2a0cdbdd
Florent Nuttens <fnuttens@dabao.fr>
2020-03-24 23:13:16 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    Ok(())
+}
