2048d54122cd1a74d3609a630e1f7f4006244901
Florent Nuttens <fnuttens@dabao.fr>
2020-03-25 17:17:52 +0100

    feat: setup camera

--- src/pong.rs
+++ src/pong.rs
@@ -13,6 +13,24 @@ use amethyst::{
     }
 };
 
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
 pub struct Pong;
 
-impl SimpleState for Pong {}
+impl SimpleState for Pong {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        initialize_camera(data.world);
+    }
+}
+
+fn initialize_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
