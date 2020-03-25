6b956c3b9a53bc1467f636a124391c435be1daa9
Florent Nuttens <fnuttens@dabao.fr>
2020-03-25 23:43:00 +0100

    feat: draw the paddles

--- src/pong.rs
+++ src/pong.rs
@@ -24,7 +24,9 @@ impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
         world.register::<Paddle>();
-        initialize_paddles(world);
+
+        let sprite_sheet_handle = load_sprite_sheet(world);
+        initialize_paddles(world, sprite_sheet_handle);
         initialize_camera(world);
     }
 }
@@ -66,7 +68,7 @@ fn initialize_camera(world: &mut World) {
         .build();
 }
 
-fn initialize_paddles(world: &mut World) {
+fn initialize_paddles(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
     let mut left_transform = Transform::default();
     let mut right_transform = Transform::default();
 
@@ -74,15 +76,44 @@ fn initialize_paddles(world: &mut World) {
     left_transform.set_translation_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
     right_transform.set_translation_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
 
+    let sprite_render = SpriteRender {
+        sprite_sheet: sprite_sheet_handle,
+        sprite_number: 0,
+    };
+
     world
         .create_entity()
         .with(Paddle::new(Side::Left))
         .with(left_transform)
+        .with(sprite_render.clone())
         .build();
 
     world
         .create_entity()
         .with(Paddle::new(Side::Right))
         .with(right_transform)
+        .with(sprite_render)
         .build();
+}
+
+fn load_sprite_sheet(world: &mut World) -> Handle<SpriteSheet> {
+    let loader = world.read_resource::<Loader>();
+
+    let texture_handle = {
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "texture/pong_spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        "texture/pong_spritesheet.ron",
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
 }
