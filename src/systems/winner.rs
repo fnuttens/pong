e83ecc60fcbc03697397aef7f3fcfc55568cf5c3
Florent Nuttens <fnuttens@dabao.fr>
2020-04-05 17:48:52 +0200

    feat(WinnerSystem): update the score board

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -4,16 +4,21 @@ use amethyst::{
     derive::SystemDesc,
     ecs::prelude::{
         Join,
+        ReadExpect,
         System,
         SystemData,
         World,
+        Write,
         WriteStorage,
     },
+    ui::UiText,
 };
 
 
 use crate::pong::{
     Ball,
+    ScoreBoard,
+    ScoreText,
     ARENA_WIDTH,
     ARENA_HEIGHT,
 };
@@ -25,25 +30,41 @@ impl<'s> System<'s> for WinnerSystem {
     type SystemData = (
         WriteStorage<'s, Ball>,
         WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
     );
 
-    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut balls, mut locals, mut ui_text, mut scores, score_text): Self::SystemData
+    ) {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
-            let did_hit = if ball_x <= ball.radius {
-                println!("Player 2 scores!");
-                true
-            } else if ball_x >= ARENA_WIDTH - ball.radius {
-                println!("Player 1 scores!");
-                true
-            } else {
-                false
-            };
+            let right_player_scored = ball_x <= ball.radius;
+            let left_player_scored = ball_x >= ARENA_WIDTH - ball.radius;
 
-            if did_hit {
+            if right_player_scored {
+                scores.score_right = (scores.score_right + 1)
+                    .min(999);
+
+                if let Some(text) = ui_text.get_mut(score_text.p2_score) {
+                    text.text = scores.score_right.to_string();
+                }
+            } else if left_player_scored {
+                scores.score_left = (scores.score_left + 1)
+                    .min(999);
+
+                if let Some(text) = ui_text.get_mut(score_text.p1_score) {
+                    text.text = scores.score_left.to_string();
+                }
+            }
+
+            if right_player_scored || left_player_scored {
                 ball.velocity[0] = -ball.velocity[0];
                 transform.set_translation_xyz(ARENA_WIDTH / 2.0, ARENA_HEIGHT / 2.0, 0.0);
+                println!("Score: | {:^3} | {:^3} |", scores.score_left, scores.score_right);
             }
         }
     }
